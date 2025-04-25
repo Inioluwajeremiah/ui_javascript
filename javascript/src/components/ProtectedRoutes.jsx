c15ed43cc20b3595b9388a98a67ab0e0fc247749
adewarainioluwa@gmail.com <adewara@gmail.com>
2025-04-25 16:53:11 +0100

    complete tutorial

--- javascript/src/components/ProtectedRoutes.jsx
+++ javascript/src/components/ProtectedRoutes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoutes = () => {
-  const isLogin = false;
+  const { userInfo, menuInfo } = useSelector((state) => state.auth);
 
-  return isLogin === false ? <Outlet /> : <Navigate to={"/signin"} replace />;
+  return userInfo ? <Outlet /> : <Navigate to={"/signin"} replace />;
 };
 
 export default ProtectedRoutes;
